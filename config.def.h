97975ffa8d27d881c148a75e8e8a64cce3d31194
sunil <sunil1996@gmail.com>
2020-05-25 22:16:13 +0530

    changed to st

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 8;        /* gaps between windows */
+static const unsigned int gappx     = 1;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -26,7 +26,6 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_gray4  },
-	//[SchemeSel]  = { col_gray4, col_red,  col_gray4 },
 	[SchemeStatus]  = { col_gray4, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
 	[SchemeTagsSel]  = { col_gray4, col_cyan,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
     [SchemeTagsNorm]  = { col_gray4,col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
@@ -75,7 +74,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_red , "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 #include <X11/XF86keysym.h>
 #include "shiftview.c"
 #include "movestack.c"
@@ -114,7 +113,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY, 			XK_n, 	   shiftview, 	   {.i = +1 } },
 	{ MODKEY, 			XK_b, 	   shiftview, 	   {.i = -1 } },
-	{ MODKEY,                       XK_q,       spawn,         SHCMD("alacritty -e lf") }, // file manager
+	{ MODKEY,                       XK_q,       spawn,         SHCMD("st -e ranger") }, // file manager
 	{ 0, XF86XK_MonBrightnessUp,	spawn,		SHCMD("/usr/bin/xbacklight -inc 5")},
 	{ 0, XF86XK_MonBrightnessDown,	spawn,		SHCMD("/usr/bin/xbacklight -dec 5")},
 	{ 0, XF86XK_AudioRaiseVolume,	spawn, 		SHCMD("/usr/bin/pactl set-sink-volume 0 +5%")},
@@ -125,9 +124,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask, 		XK_b,      spawn, 		SHCMD("/usr/local/bin/book.sh")},
 	{ MODKEY|ShiftMask, 		XK_y,      spawn, 		SHCMD("/usr/bin/firefox https://youtube.com")},
 	{ MODKEY|ShiftMask, 		XK_h,      spawn, 	       SHCMD("/usr/local/bin/htop_scratch.sh")},
-	{ MODKEY, 		        XK_Up,     scratchpad_show, 	{0}                                          },
-	{ MODKEY, 		        XK_Down,   scratchpad_hide,     {0}                                          },
-	{ MODKEY|ShiftMask, 		XK_Down,   scratchpad_remove,   {0}                               	     },
+	{ MODKEY, 		    XK_grave,      spawn, 	       SHCMD("/usr/local/bin/st_scratch.sh")},
+	{ MODKEY|ControlMask, 	       XK_j,     scratchpad_show, 	{0}                                          },
+	//{ MODKEY|ControlMask, 	       XK_k,   scratchpad_hide,     {0}                                          },
+	{ MODKEY|ShiftMask|ControlMask, XK_k,   scratchpad_remove,   {0}                               	     },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
